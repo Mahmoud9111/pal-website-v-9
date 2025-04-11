aa7cb270906ae9cba8ceaf7f4c07ba06d5982e8f
Mahmoud9111 <mahmoodjahshan8@gmail.com>
2025-04-11 21:41:46 +0200

    first commit

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -22,7 +22,7 @@ const cards = [
 
 function ProjectsCard() {
   return (
-    <div className="flex gap-4 flex-wrap justify-center max-w-[1536px] mx-auto p-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-[1536px] mx-auto p-4">
       {cards.map((card, index) => (
         <Link to={`/projects/${card.id}`} key={index}>
           <motion.div 
@@ -45,19 +45,19 @@ function ProjectsCard() {
               mass: 0.1,
               delay: 0.05
             }}
-            className="relative w-[490px] h-[450px] rounded-2xl overflow-hidden group cursor-pointer"
+            className="relative w-full h-[300px] md:h-[350px] lg:h-[450px] rounded-2xl overflow-hidden group cursor-pointer"
           >
             <img 
               src={card.image} 
               alt={card.title}
-              className="w-full h-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-107"
+              className="w-full h-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-105"
             />
-            <div className="absolute inset-0 bg-black/20 bg-opacity-40 flex items-end p-6">
+            <div className="absolute inset-0 bg-black/20 bg-opacity-40 flex items-end p-4 md:p-6">
               <div className="flex justify-between items-center w-full">
-                <h3 className="text-white font-medium">{card.title}</h3>
-                <div className="absolute bottom-0 right-0 py-4 px-6 transition-transform duration-300 group-hover:translate-x-2">
-                  <span className="ml-2 bg-white rounded-full p-2 sm:p-3 inline-flex items-center justify-center transition-transform duration-300 group-hover:translate-x-1 group-hover:-translate-y-1">
-                    <ArrowUpRight className="h-4 w-4 sm:h-5 sm:w-5 text-black" />
+                <h3 className="text-white text-base md:text-lg lg:text-xl font-medium">{card.title}</h3>
+                <div className="absolute bottom-0 right-0 py-3 md:py-4 px-4 md:px-6 transition-transform duration-300 group-hover:translate-x-2">
+                  <span className="ml-2 bg-white rounded-full p-2 inline-flex items-center justify-center transition-transform duration-300 group-hover:translate-x-1 group-hover:-translate-y-1">
+                    <ArrowUpRight className="h-4 w-4 text-black" />
                   </span>
                 </div>
               </div>
